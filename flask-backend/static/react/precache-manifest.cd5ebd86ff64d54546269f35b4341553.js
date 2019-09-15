49bf499cf31deeb4fc28fa3c9e44e4b2c731ec64
Ashvini Kumar <ashvini@esthenos.com>
2019-09-15 16:25:26 +0530

    react getting served from flask

---
+++ flask-backend/static/react/precache-manifest.cd5ebd86ff64d54546269f35b4341553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d03a535bfefae9bd25b0fe7ebc58bef",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "38d036fe5fd6da7effb2",
+    "url": "/static/react/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7af90ad4054f91915c43",
+    "url": "/static/react/js/2.8c1e770a.chunk.js"
+  },
+  {
+    "revision": "38d036fe5fd6da7effb2",
+    "url": "/static/react/js/main.7cb8aeeb.chunk.js"
+  },
+  {
+    "revision": "c4ecb7746c24a6809a4f",
+    "url": "/static/react/js/runtime~main.2a22b46e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
